64e13eaab4419445e71cdbade353bef2fcb1fb35
Amir Yousef Heydari <yousef@idmelon.co>
2022-01-24 21:25:19 +0330

    api data added

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const { MongoClient } = require('mongodb');
-module.exports = async function connect() {
-  const uri = '';
+module.exports.connect = async () => {
+  const uri =
+    'mongodb+srv://admin:admin@cluster0.6siop.mongodb.net/FLIGHTS?retryWrites=true&w=majority';
   await new MongoClient(uri).connect();
 };
