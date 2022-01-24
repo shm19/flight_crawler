c90e8509f6f3c410e70f25c3d51fab12c726438e
Shm <malekishayan79@gmail.com>
2022-01-24 21:15:43 +0330

    change express

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const createError = require('http-errors');
-const http = require('http');
 const api = require('./routes/api');
 const db = require('./db');
 const { getData } = require('./data');
@@ -12,26 +11,18 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', api);
+app.use('/api', api);
 
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-app.use(function (err, req, res, next) {
-  console.log(err);
-});
+app.use((err, req, res, next) =>
+  res.status(500).json({
+    status: 'error',
+    message: 'Something went wrong',
+    err,
+  })
+);
 
-app.set('port', 3000);
-
-/**
- * Create HTTP server.
- */
-
-const server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(3000, () => console.log('server started on port 3000...'));
+app.listen(3000, () => console.log('Server is running on port 3000'));
