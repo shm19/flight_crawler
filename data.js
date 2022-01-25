a5299b2abc1dd24bb11bfb68da4b553c158312bf
Amir Yousef Heydari <yousef@idmelon.co>
2022-01-25 22:16:15 +0330

    blah

--- data.js
+++ data.js
@@ -6,12 +6,15 @@ module.exports.getData = async () => {
       'https://www.kayak.com/s/horizon/exploreapi/destinations?airport=THR&budget=&depart=20220124&return=20220128&duration=&exactDates=true&flightMaxStops=&stopsFilterActive=false&topRightLat=37.69751058611946&topRightLon=78.29572187500048&bottomLeftLat=33.62902557059839&bottomLeftLon=24.330878124999572&zoomLevel=4&selectedMarker=&themeCode=&selectedDestination='
     );
     const array = a.data.destinations;
+    const types = ['normal', 'firstClass'];
+
     const result = [];
     array.forEach((element) => {
       result.push({
         // date: null,
         // air_line: null,
         // flightnumber: 'IR769',
+        flight_type: types[Math.floor(Math.random() * 2)],
         origin_air_port: element.originAirportShortName,
         destination_air_port: element.airport.shortName,
         origin_city: 'Tehran',
